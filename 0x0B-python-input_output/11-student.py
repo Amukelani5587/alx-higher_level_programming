c835ceabbfd94ce89b71122d599ed54e75d6eaec
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-10 14:54:19 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
 """Python function to create class Student"""
+#!/usr/bin/python3
 
+
+class Student:
+    """class with methods to_json for retrieves dictionary"""
+
+    def __init__(self, first_name, last_name, age):
+        """method for initialized all atributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method for retrieve a dictionary representation for a
+        student instance"""
+
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for key, value in self.__dict__.items():
+            for i in attrs:
+                if key == i:
+                    dic[key] = value
+        return dic
+
+    def reload_from_json(self, json):
+        ''' method that replaces all atrributes of the Student instance'''
+
+        for j_key, j_value in json.items():
+            self.__dict__[j_key] = j_value
 
 class Student:
     def __init__(self, first_name, last_name, age):
