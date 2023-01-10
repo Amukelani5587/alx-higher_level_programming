c465952c30d76717f74ccc0266c6a5febaf08ec4
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-10 14:59:12 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 """Python function to create class Student"""
-#!/usr/bin/python3
 
 
 class Student:
-    """class with methods to_json for retrieves dictionary"""
-
     def __init__(self, first_name, last_name, age):
-        """method for initialized all atributes"""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+        
     def to_json(self, attrs=None):
-        """method for retrieve a dictionary representation for a
-        student instance"""
-
         if attrs is None:
             return self.__dict__
         dic = {}
@@ -31,12 +23,3 @@ class Student:
 
         for j_key, j_value in json.items():
             self.__dict__[j_key] = j_value
-
-class Student:
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        return self.
