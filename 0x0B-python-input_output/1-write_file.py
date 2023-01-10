46ab9f44adb3eea65daf21c0789fe8de3791c326
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-10 14:43:51 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''Python Function to count number of lines in a file'''
+'''Python Function to write text to file and
+   returns number of characters written
+'''
 
 
-def number_of_lines(filename=""):
-    count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            count += 1
-    return count
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
