127c714b8417caa9bcb32f36ac00282c626ec3dc
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-11 17:13:40 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Python function that add arguments to a Python list
-"""
+
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import os
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-
-if not os.path.exists("./add_item.json"):
-    save_json(my_list, "add_item.json")
-my_list = load_json("add_item.json")
-
-for items in sys.argv[1:]:
-    my_list.append(items)
-save_json(my_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
