b45d2c97a40ec232841b81fdde3c9e99d00346d0
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-11 16:13:10 +0200

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""Python Function to append string to a file"""
+# 4-append_write.py
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding='utf-8') as f:
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
