f7a79d255f2fcd9c2fb26a5948f3639cb9649c1a
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-13 14:58:46 +0200

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+'''Square class module'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Instantiate Square class attributes'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Instantiation of Square attriutes'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''print attribute with __str__'''
+        a, b, c, d = self.id, self.x, self.y, self.size
+        return ("[Square] ({}) {}/{} - {}".format(a, b, c, d))
+
+    def update(self, *args, **kwargs):
+        '''Set up args for rectangle'''
+        if len(args) != 0:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.id = args[0]
+                self.size = args[1]
+            if len(args) == 3:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+            if len(args) == 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+        else:
+            for key, values in kwargs.items():
+                setattr(self, key, values)
+
+    @property
+    def size(self):
+        """set the property of width"""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        '''Method to set size'''
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        else:
+            self.height = value
+
+    def to_dictionary(self):
+        '''Method to return dictionary representation of square'''
+        dic = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return dic
