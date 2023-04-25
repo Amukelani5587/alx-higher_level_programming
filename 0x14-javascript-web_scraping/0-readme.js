7192815bb5489196ead8a3880d5ea5bb24c90acc
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-04-25 18:43:42 +0200

    Reads a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-const NameFile = process.argv[2];
 
-if (NameFile) {
-  fs.readFile(NameFile, 'utf8', (error, datos) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(datos);
-    }
-  });
-}
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
