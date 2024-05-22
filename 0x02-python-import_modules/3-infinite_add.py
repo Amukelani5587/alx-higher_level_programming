b78c191b09d799da5edda63d93ca756d6c9a212e
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2024-05-23 01:19:01 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
-
-    tot = 0
-    for arg in sys.argv[1:]:
-        tot += int(arg)
-    print("{:d}".format(tot))
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
